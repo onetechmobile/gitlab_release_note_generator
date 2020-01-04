9d60fe177cd6a839857862f7858c09712ceb7385
Ayan <ayan.kurmanbai@gmail.com>
2020-01-04 19:43:05 +0600

    deleted files

--- lib/gitlab_release_note_generator/version.rb
+++ lib/gitlab_release_note_generator/version.rb
@@ -1,3 +1,3 @@
 module GitlabReleaseNoteGenerator
-  VERSION = "0.1.0"
+  VERSION = "0.1.1"
 end
--- spec/notes_generator_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'gitlab_release_note_generator'
-
-describe GitlabReleaseNoteGenerator::Builder do
-    it "broccoli is gross" do
-        expect(GitlabReleaseNoteGenerator::Builder.testFunc("abc")).to eql("abc")
-    end
-
-    puts "Here"
-end
