1f13984de4b1ad632fcb044136f6e03193328e3a
Ayan <ayan.kurmanbai@gmail.com>
2020-01-06 16:39:30 +0600

    added input

--- lib/gitlab_release_note_generator.rb
+++ lib/gitlab_release_note_generator.rb
@@ -4,5 +4,9 @@ require "gitlab_release_note_generator/builder"
 module GitlabReleaseNoteGenerator
   class Error < StandardError; end
   builder = GitlabReleaseNoteGenerator::Builder.new
+  host_name = ARGV[0]
+  private_token = ARGV[1]
+  project_id = ARGV[2] 
+
   builder.main
 end
