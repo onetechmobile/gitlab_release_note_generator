e03883331e7c273d9702179c60eb085550824814
Ayan <ayan.kurmanbai@gmail.com>
2020-01-07 19:18:00 +0600

    fixed builder

--- lib/gitlab_release_note_generator/builder.rb
+++ lib/gitlab_release_note_generator/builder.rb
@@ -1,20 +1,21 @@
 require 'faraday'
 require 'json'
+require 'date'
 
 module GitlabReleaseNoteGenerator
     class Builder
-        @base_url = 'https://'
+        @@base_url = 'https://'
 
         def main(host_name, private_token, project_id)       
-            base_url += "#{host_name}/api/v4/projects/#{project_id}"
-            conn = Faraday.new(:url => base_url) do |faraday|
+            @@base_url << "#{host_name}/api/v4/projects/#{project_id}"
+            conn = Faraday.new(:url => @@base_url) do |faraday|
                 faraday.headers['PRIVATE-TOKEN'] = private_token
             end
             get_tags(conn)
         end
 
         def get_tags(conn) 
-            response = conn.get base_url + '/repository/tags'
+            response = conn.get @@base_url + '/repository/tags'
             tags_dict = JSON.parse(response.body)
             tag_name = tags_dict[0]["name"]
 
@@ -26,20 +27,22 @@ module GitlabReleaseNoteGenerator
         end
 
         def get_all_merge_request_before(conn, datetime, tag) 
-            response = conn.get base_url + "/merge_requests?created_before=#{datetime}"
+            response = conn.get @@base_url + "/merge_requests?created_before=#{datetime}"
             generate_changelog(conn, response.body, tag)
         end
 
         def get_all_merge_requests_after(conn, datetime, tag)
-            response = conn.get base_url + "/merge_requests?created_after=#{datetime}"
+            response = conn.get @@base_url + "/merge_requests?created_after=#{datetime}"
             generate_changelog(conn, response.body, tag)
         end
 
         def generate_changelog(conn, response_body, tag)  
             merge_requests = JSON.parse(response_body)
-            description = '### Release note (date)\r\n#### Closed merge requests\r\n'
+            description = '### Merged merge requests (%s)\r\n#### Closed merge requests\r\n'
+            description = description % [getCurrentTime]
             for merge in merge_requests
-                description += "- #{merge["title"]} " + "[#{merge["reference"]}]" + "(#{merge["web_url"]})" + '\r\n'
+                description += "- #{merge["title"]} " + "[#{merge["reference"]}]" + "(#{merge["web_url"]})"
+                description += " [#{merge["author"]["username"]}]" + "(#{merge["author"]["web_url"]})" + '\r\n'
             end    
             
             post_new_release(conn, description, tag)
@@ -50,12 +53,17 @@ module GitlabReleaseNoteGenerator
             res_body = body % [tag, tag, description]
             puts res_body
             response = conn.post do |req|
-                req.url 'https://gitlab.com/api/v4/projects/16087798/releases'
+                req.url @@base_url + '/releases'
                 req.headers['Content-Type'] = 'application/json'
                 req.body = res_body
             end
             
             puts response.status, response.body
+        end
+
+        def getCurrentTime
+            current_time = Time.now.strftime("%d.%m.%Y")
+            return current_time
         end
     end
 end
