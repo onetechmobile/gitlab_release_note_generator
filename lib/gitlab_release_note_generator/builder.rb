3fec0aa6e3aa0071f85df618e6be313a6a1e7a7d
Ayan <ayan.kurmanbai@gmail.com>
2020-01-08 14:21:19 +0600

    fixed bugs, added issues

--- lib/gitlab_release_note_generator/builder.rb
+++ lib/gitlab_release_note_generator/builder.rb
@@ -20,33 +20,87 @@ module GitlabReleaseNoteGenerator
             tag_name = tags_dict[0]["name"]
 
             if tags_dict.count > 1 
-                get_all_merge_requests_after(conn, tags_dict[1]["commit"]["created_at"], tag_name)
+                merged_requests = get_all_merge_requests_after(conn, tags_dict[1]["commit"]["created_at"])
+                closed_issues = get_all_issues_after(conn, tags_dict[1]["commit"]["created_at"])
+                generate_changelog(conn, tag_name, merged_requests, closed_issues)
             else
-                get_all_merge_request_before(conn, tags_dict[0]["commit"]["created_at"], tag_name)    
+                merged_requests = get_all_merge_request_before(conn, tags_dict[0]["commit"]["created_at"])    
+                closed_issues = get_all_issues_before(conn, tags_dict[0]["commit"]["created_at"]) 
+                generate_changelog(conn, tag_name, merged_requests, closed_issues)
+            end            
+        end
+
+        def get_all_merge_request_before(conn, datetime) 
+            response = conn.get @@base_url + "/merge_requests?created_before=#{datetime}?state=merged?scope=all"
+            return response.body
+        end
+
+        def get_all_merge_requests_after(conn, datetime)
+            response = conn.get @@base_url + "/merge_requests?created_after=#{datetime}?state=merged?scope=all"
+
+            return response.body
+        end
+
+        def get_all_issues_before(conn, datetime)
+            response = conn.get @@base_url + "/issues?created_before=#{datetime}?state=closed?scope=all"    
+            return response.body
+        end
+
+        def get_all_issues_after(conn, datetime) 
+            response = conn.get @@base_url + "/issues?created_after=#{datetime}?state=closed?scope=all" 
+            return response.body
+        end 
+
+        def generate_changelog(conn, tag, merged_requests_response_body, closed_issues_response_body)  
+            merged_requests = JSON.parse(merged_requests_response_body)
+            closed_issues = JSON.parse(closed_issues_response_body)
+            description = '### Release note (%s)\r\n'
+            description = description % [getCurrentTime]        
+
+            closed_issues_notes = generate_closed_issues_notes(conn, closed_issues)
+            merged_requests_notes = generate_merged_requests_notes(conn, merged_requests)   
+            
+            if closed_issues_notes.nil? && merged_requests_notes.nil? 
+                puts "Didn't find either closed issues or merged requests between last two tags."
+                return
             end
-        end
+            
+            if !closed_issues_notes.nil? 
+                description += closed_issues_notes
+            end
 
-        def get_all_merge_request_before(conn, datetime, tag) 
-            response = conn.get @@base_url + "/merge_requests?created_before=#{datetime}"
-            generate_changelog(conn, response.body, tag)
-        end
-
-        def get_all_merge_requests_after(conn, datetime, tag)
-            response = conn.get @@base_url + "/merge_requests?created_after=#{datetime}"
-            generate_changelog(conn, response.body, tag)
-        end
-
-        def generate_changelog(conn, response_body, tag)  
-            merge_requests = JSON.parse(response_body)
-            description = '### Merged merge requests (%s)\r\n#### Closed merge requests\r\n'
-            description = description % [getCurrentTime]
-            for merge in merge_requests
-                description += "- #{merge["title"]} " + "[#{merge["reference"]}]" + "(#{merge["web_url"]})"
-                description += " [#{merge["author"]["username"]}]" + "(#{merge["author"]["web_url"]})" + '\r\n'
-            end    
+            if !merged_requests_notes.nil? 
+                description += merged_requests_notes
+            end 
             
             post_new_release(conn, description, tag)
-        end 
+        end
+
+        def generate_merged_requests_notes(conn, merged_requests) 
+            if merged_requests.count == 0 
+                return nil
+            end 
+            description = '#### Closed merge requests\r\n'
+            for merge in merged_requests
+                description += "- #{merge["title"]} " + "[##{merge["reference"]}]" + "(#{merge["web_url"]})"
+                description += " ([#{merge["author"]["username"]}]" + "(#{merge["author"]["web_url"]}))" + '\r\n'
+            end    
+
+            return description
+        end
+
+        def generate_closed_issues_notes(conn, closed_issues) 
+            if closed_issues.count == 0 
+                return nil
+            end 
+            description = '#### Closed issues\r\n' 
+            for issue in closed_issues
+                description += "- #{issue["title"]} " + "[##{issue["iid"]}]" + "(#{issue["web_url"]})"
+                description += " ([#{issue["author"]["username"]}]" + "(#{issue["author"]["web_url"]}))" + '\r\n'
+            end 
+            
+            return description
+        end
 
         def post_new_release(conn, description, tag) 
             body = '{ "name":  "%s", "tag_name": "%s", "description": "%s"}'
